582327a99111f18c615196ff309a59dd37c48bff
lextri980 <lextri980@gmail.com>
2023-06-29 04:26:38 +0700

    update

---
+++ Sec14/lecture/script.js
@@ -0,0 +1,11 @@
+const con = "con";
+const cat = "cat";
+const longText = 'This is a long text'
+
+// console.log(con + " " + cat);
+
+// console.log(`${con} ${cat}`);
+
+const method = longText.split('s')
+
+console.log(method)
